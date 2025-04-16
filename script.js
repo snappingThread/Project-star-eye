bc2e71e3dcfb4873dd64d60d1091d5d79873b3df
snappingThread <snappingthread@gmail.com>
2025-04-16 16:50:10 +0100

    Update script.js

--- script.js
+++ script.js
@@ -1,5 +1,3 @@
-import * as THREE from "https://cdn.jsdelivr.net/npm/three@0.152.0/build/three.module.js";
-
 // Fetch data from Celestrak
 const url = "https://www.celestrak.com/NORAD/elements/stations.txt";
 
